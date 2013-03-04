4ebd42ae4affad688e391f1c89591ef5d45302c7
Tim Irwin <tim.irwin@rolemodelsoftware.com>
2013-03-04 17:06:32 -0500

    Add Rails Engine

--- lib/calendar_date_select.rb
+++ lib/calendar_date_select.rb
@@ -2,45 +2,12 @@ require "calendar_date_select/calendar_date_select.rb"
 require "calendar_date_select/form_helpers.rb"
 require "calendar_date_select/includes_helper.rb"
 
-require 'rails'
-require 'active_support'
-
 module CalendarDateSelect
-
-  Files = [
-    '/app/assets',
-    '/app/assets/javascripts/calendar_date_select',
-    '/app/assets/stylesheets/calendar_date_select',
-    '/app/assets/images/calendar_date_select',
-    '/app/assets/javascripts/calendar_date_select/locale'
-  ]
-
-  class Railtie < ::Rails::Railtie
-
-    initializer 'calendardateselect.initialize', :after => :action_view do
+  class Engine < Rails::Engine
+    config.after_initialize do
       ActionView::Helpers::FormHelper.send(:include, CalendarDateSelect::FormHelpers)
       ActionView::Base.send(:include, CalendarDateSelect::FormHelpers)
       ActionView::Base.send(:include, CalendarDateSelect::IncludesHelper)
-
-      ActionView::Helpers::InstanceTag.class_eval do
-        class << self; alias new_with_backwards_compatibility new; end #TODO: singleton_class.class_eval
-      end
     end
-
-    rake_tasks do
-      namespace :calendar do
-        desc "Install assets required by calendar_date_select gem"
-        task :install do
-
-          Files.each do |f|
-            source = File.join(File.dirname(__FILE__), "..", f)
-            dest = File.join(Rails.root, f)
-            FileUtils.mkdir_p(dest, :verbose => true)
-            FileUtils.cp(Dir.glob(source+'/*.*'), dest, :verbose => true)
-          end
-
-        end # task
-      end #namespace
-    end # rake_tasks
-  end # Railtie
-end # module
+  end
+end
